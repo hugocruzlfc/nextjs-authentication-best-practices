c44d5eefe29ad50d821e77e60481d9909071852d
hugocruzlfc <hugocruzlfc@gmail.com>
2024-07-02 12:55:55 +0200

    feat: add prisma client config

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,12 @@
+import { NextPage } from "next";
+import { SignUpForm } from "./form";
+
+const Page: NextPage = () => {
+  return (
+    <div>
+      <SignUpForm />
+    </div>
+  );
+};
+
+export default Page;
